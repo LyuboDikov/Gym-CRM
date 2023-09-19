73fab082da236bee10a86890e229effead161e32
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-19 16:22:44 +0300

    Created User, Record and Report repositories

---
+++ GymCRM/src/main/java/com/example/gymcrm/repository/RecordRepository.java
@@ -0,0 +1,9 @@
+package com.example.gymcrm.repository;
+
+import com.example.gymcrm.model.entity.RecordEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<RecordEntity, Long> {
+}
---
+++ GymCRM/src/main/java/com/example/gymcrm/repository/ReportRepository.java
@@ -0,0 +1,9 @@
+package com.example.gymcrm.repository;
+
+import com.example.gymcrm.model.entity.ReportEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
+}
